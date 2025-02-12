2c21c7b3deb3fc5787a368e397803353f0c43d1b
opipoy <98356389+opipoy@users.noreply.github.com>
2025-02-12 22:17:34 +0000

    Update build.sh

--- shower_svelte/build.sh
+++ shower_svelte/build.sh
@@ -3,6 +3,7 @@ npm run build
 npx cap sync android
 cd android
 ./gradlew assembleDebug
-nc -l -p 9659 < ./app/build/outputs/apk/debug/app-debug.apk
-ls -l ./app/build/outputs/apk/debug/app-debug.apk
+# nc -l -p 9659 < ./app/build/outputs/apk/debug/app-debug.apk
+# ls -l ./app/build/outputs/apk/debug/app-debug.apk
+echo "app can be found in ./app/build/outputs/apk/debug/app-debug.apk"
 cd ..
